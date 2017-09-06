540817c6413fe2a490669f31d49fda6233e4fcde
Yoichi Hirai <i@yoichihirai.com>
2017-09-06 15:10:56 +0200

    Document the interface

--- rlp.mli
+++ rlp.mli
@@ -1,8 +1,12 @@
+(** [t] is the type for RLP tree structures. *)
 type t =
   RlpData of Rope.t
 | RlpList of t list
 
+(** [encode t] encodes the RLP tree structure into a byte string represented as a Rope.t *)
 val encode : t -> Rope.t
 
 exception InvalidRlp
+
+(** [decode r] reads the given byte string into the RLP tree structure. Raises [InvalidRlp] when [r] is not a "canonical" encoding. [decode (encode t)] should be equal to [t] up to the Rope content equality. *)
 val decode : Rope.t -> t
