bac6505377b8b5005c736bcc052af69c012b45da
Yoichi Hirai <i@yoichihirai.com>
2017-09-06 17:02:54 +0200

    Enable ocaml setup.ml -doc

--- rlp.mli
+++ rlp.mli
@@ -1,6 +1,6 @@
-(** Rlp: Recursive Length Prefix format. **)
+(** Rlp: Recursive Length Prefix format. *)
 
-(** {2} RLP Trees *)
+(** {2 RLP Trees} *)
 
 (** [t] is the type for RLP tree structures. *)
 type t =
@@ -8,11 +8,11 @@ type t =
 | RlpList of t list
 
 (** [rlpInt i] encodes [i] into an [RlpData] element with big-endian with no zeros in front.  Zero is encoded as the empty byte sequence.
- *  Raises [Invalid_argument] when [i] is negative.
+    Raises [Invalid_argument] when [i] is negative.
  *)
 val rlpInt : int -> t
 
-(** [rlpBigInt] is similar to [rlpInt]. **)
+(** [rlpBigInt] is similar to [rlpInt]. *)
 val rlpBigInt : Big_int.big_int -> t
 
 (** [displayRope t] converts the RLP tree [t] into a JSON-like string (represented as a [Rope.t]). *)
@@ -21,7 +21,7 @@ val displayRope : t -> Rope.t
 (** [displayRope t] converts the RLP tree [t] into a JSON-like string (represented as a [Rope.t]). *)
 val display : t -> string
 
-(** {2} Encoding and Decoding *)
+(** {2 Encoding and Decoding} *)
 
 (** [encode t] encodes the RLP tree structure into a byte string represented as a Rope.t *)
 val encode : t -> Rope.t
